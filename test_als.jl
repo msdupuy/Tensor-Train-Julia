9827ab1f1f482d128d9742b702ce4c7128087ef8
Mi-Song <dupuy@ma.tum.de>
2020-11-30 12:14:52 +0100

    iterative solver test added

--- test_als.jl
+++ test_als.jl
@@ -1,6 +1,5 @@
 include("als.jl")
 using Plots
-plotlyjs()
 
 function Lap(n::Integer,d::Integer) #returns the tensor of the discrete Laplacian in a box [0,1]^d with n equidistant discretization points in each direction
     A = zeros(n^d,n^d)    
@@ -32,7 +31,7 @@ function test_als()
     b_tt = ttv_decomp(b,1)
     x0_tt = ttv_decomp(x0,1)
     x = L\b[:]
-    x_tt = als(L_tt,b_tt,x0_tt,[n,n,1])
+    x_tt = als(L_tt,b_tt,x0_tt)
     y = ttv_to_tensor(x_tt)
     @test isapprox(y[:],x)
 end
@@ -41,14 +40,14 @@ function test_als_eig()
     n = 10
     L = Lap(n,3)
     E = eigmin(L)
-    b = ones(n,n,n)
     x0 = randn(n,n,n)
     L_tt = tto_decomp(reshape(L,n,n,n,n,n,n),1)
-    b_tt = ttv_decomp(b,1)
     x0_tt = ttv_decomp(x0,1)
     E_tt,x_tt = als_eig(L_tt,x0_tt)
     y = ttv_to_tensor(x_tt)
     @test isapprox(E,E_tt)
+    E_tt,x_tt = als_eig(L_tt,x0_tt,it_solver=true)
+    @test isapprox(E,E_tt,atol=1e-8)
 end
 
 function test_als_gen_eig()
@@ -58,13 +57,13 @@ function test_als_gen_eig()
     S = randn(n^3,n^3)
     S = S*S'+I
     F = eigen(L,S)
-    b = ones(n,n,n)
     x0 = randn(n,n,n)
     L_tt = tto_decomp(reshape(L,n,n,n,n,n,n),1)
     S_tt = tto_decomp(reshape(S,n,n,n,n,n,n),1)
-    b_tt = ttv_decomp(b,1)
     x0_tt = ttv_decomp(x0,1)
-    E_tt,x_tt = als_gen_eig(L_tt,S_tt,x0_tt,[n,n,1])
+    E_tt,x_tt = als_gen_eig(L_tt,S_tt,x0_tt)
     y = ttv_to_tensor(x_tt)
     @test isapprox(real(F.values[1]),E_tt)
+    E_tt,x_tt = als_gen_eig(L_tt,S_tt,x0_tt,it_solver=true)
+    @test isapprox(real(F.values[1]),E_tt,atol=1e-8)
 end
