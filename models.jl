3941ab6c6ac62384925136a651d552e2d782779a
Mi-Song <dupuy@ma.tum.de>
2020-12-08 19:03:31 +0100

    it seems to work

--- models.jl
+++ models.jl
@@ -133,30 +133,16 @@ end
 
 #returns the h,V for the hubbard model
 #odd index = spin up, even index = spin down
-function hubbard_1D(L;t=1,U=1)
-    h = zeros(2L,2L)
-    V = zeros(2L,2L,2L,2L)
-    for i in 1:L-1
-        h[2i,2i+2]=-t
-        h[2i-1,2i+1]=-t
-        V[2i-1,2i,2i-1,2i]=-U #because of the anticommutation rules
-    end
-    V[2L-1,2L,2L-1,2L]=-U
-    return h,V
-end
 
 function half_filling(N)
-    x = zeros(Int,2N)
-    for i in 0:N-1
-        if isodd(i)
-            x[2i+1] = 1
-        else
-            x[2i+2] = 1
-        end
+    tt_vec = Array{Array{Float64,3},1}(undef,2N)
+    for i in 1:N
+        tt_vec[2i-1] = zeros(2,1,1)
+        tt_vec[2i-1][1,1,1] = 1.0
+        tt_vec[2i] = zeros(2,1,1)
+        tt_vec[2i][2,1,1] = 1.0
     end
-    ψ = sparsevec([undigits(x)],[1.0],2^(2N))
-    ψ_tt = ttv_spdecomp(sparsetensor_vec(2*ones(Int,2N),ψ),2N)
-    return ψ_tt
+    return ttvector(tt_vec,2*ones(Int,2N),ones(Int,2N),zeros(2N))
 end
 
 #auxiliary functions for a_p^†a_q
@@ -224,19 +210,15 @@ end
 #assume k<l,m<n
 function two_body_mpo(k,l,m,n,L)
     H = Array{Array{Float64,4},1}(undef,L) 
-    if [k,l] == [m,n]
-        return one_body_mpo(k,l,L)
-    elseif k in [m,n] || l in [m,n]
-        x = symdiff([k,l],[m,n])
-        if x[1] in [k,l]
-            return one_body_mpo(x[1],x[2],L)
-        else
-            return one_body_mpo(x[2],x[1],L)
-        end
+    if l == m
+        A = one_body_mpo(k,m,L)
+        B = one_body_mpo(l,n,L)
+        C = one_body_mpo(k,n,L)
+        return tto_add(mult_a_tt(-1.0,mult(A,B)),C)
     else
         A = one_body_mpo(k,m,L)
         B = one_body_mpo(l,n,L)
-        return mult(A,B)
+        return mult_a_tt(-1.0,mult(A,B))
     end
 end
 
@@ -247,6 +229,42 @@ function test_2body_mpo()
     Hmpo = two_body_mpo(k,l,m,n,L)
     H2 = tto_to_tensor(Hmpo)
     @test isapprox(norm(H-reshape(H2,2^L,2^L)),0.0,atol=1e-10)
+end
+
+#assuming natural labelling of sites
+function hubbard_1D(L;t=1,U=1)
+    h = zeros(2L,2L)
+    V = zeros(2L,2L,2L,2L)
+    for i in 1:L-1
+        h[2i,2i+2]=-t
+        h[2i-1,2i+1]=-t
+        V[2i-1,2i,2i-1,2i]=-U #because of the anticommutation rules
+    end
+    h[2L-1,1]=-t
+    h[2L,2]=-t
+    V[2L-1,2L,2L-1,2L]=-U
+    return h,V
+end
+
+#assuming diagonal terms are divided by 2 in the h and V matrix
+function hV_to_mpo(h,V)
+    L = size(h,1)
+    i_list = findall(!iszero,h)
+    if length(i_list)>0
+        i = i_list[1]
+        A = mult_a_tt(h[i],tto_add(one_body_mpo(i[1],i[2],L),one_body_mpo(i[2],i[1],L)))
+        for i in i_list[2:end]
+            H = one_body_mpo(i[1],i[2],L)
+            G = one_body_mpo(i[2],i[1],L)
+            A = tto_add(A,mult_a_tt(h[i],tto_add(G,H)))
+        end
+    end
+    for i in findall(!iszero,V)
+        H = two_body_mpo(i[1],i[2],i[3],i[4],L)
+        G = two_body_mpo(i[4],i[3],i[2],i[1],L)
+        A = tto_add(A,mult_a_tt(V[i],tto_add(H,G)))
+    end
+    return A
 end
 
 function ham_to_mpo(h,V,N)
