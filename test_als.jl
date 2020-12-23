0f46ef55f58ee8f1068bfbc7196978b9e0f7ec8b
Mi-Song <dupuy@ma.tum.de>
2020-12-23 14:33:04 +0100

    added mals test

--- test_als.jl
+++ test_als.jl
@@ -1,4 +1,5 @@
 include("als.jl")
+include("mals.jl")
 using Plots
 
 function Lap(n::Integer,d::Integer) #returns the tensor of the discrete Laplacian in a box [0,1]^d with n equidistant discretization points in each direction
@@ -78,4 +79,18 @@ function test_als_gen_eig()
     @test isapprox(real(F.values[1]),E_tt[end])
     E_tt,x_tt = als_gen_eig(L_tt,S_tt,x0_tt,it_solver=true)
     @test isapprox(real(F.values[1]),E_tt[end],atol=1e-8)
+end
+
+function test_mals()
+    n = 10
+    L = Lap(n,3)
+    b = ones(n,n,n)
+    x0 = randn(n,n,n)
+    L_tt = tto_decomp(reshape(L,n,n,n,n,n,n),1)
+    b_tt = ttv_decomp(b,1)
+    x0_tt = ttv_decomp(x0,1)
+    x = L\b[:]
+    x_tt = mals(L_tt,b_tt,x0_tt,1e-12)
+    y = ttv_to_tensor(x_tt)
+    @test isapprox(y[:],x)
 end
