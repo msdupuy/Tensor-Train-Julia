487bae2d50bb0251a1e50bb759a3b338716c8421
Mi-Song <dupuy@ma.tum.de>
2020-12-24 16:06:21 +0100

    test_mals()

--- test_als.jl
+++ test_als.jl
@@ -90,7 +90,21 @@ function test_mals()
     b_tt = ttv_decomp(b,1)
     x0_tt = ttv_decomp(x0,1)
     x = L\b[:]
-    x_tt = mals(L_tt,b_tt,x0_tt,1e-12)
+    x_tt = mals(L_tt,b_tt,x0_tt;tol=0.0)
     y = ttv_to_tensor(x_tt)
     @test isapprox(y[:],x)
+end
+
+function test_mals_eig()
+    n = 10
+    L = Lap(n,3)
+    E = eigmin(L)
+    x0 = randn(n,n,n)
+    L_tt = tto_decomp(reshape(L,n,n,n,n,n,n),1)
+    x0_tt = ttv_decomp(x0,1)
+    E_tt,x_tt = mals_eig(L_tt,x0_tt)
+    y = ttv_to_tensor(x_tt)
+    @test isapprox(E,E_tt[end])
+    E_tt,x_tt = mals_eig(L_tt,x0_tt,it_solver=true)
+    @test isapprox(E,E_tt[end],atol=1e-8)
 end
