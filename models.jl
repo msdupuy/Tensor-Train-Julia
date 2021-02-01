5cea0bf221d45944849f5f3ce8239b1307f50e19
Mi-Song Dupuy <misong.dupuy@gmail.com>
2021-02-01 11:22:59 +0100

    moved functions in sp_models.jl

--- models.jl
+++ models.jl
@@ -11,6 +11,10 @@ TODO: a_i†a_j†a_ka_l in MPO, tto_add function and hubbard_1d/2d?
 test with aklt model
 """
 
+"""
+one_body_to_matrix and two_body_to_matrix for testing purposes
+"""
+
 function one_body_to_matrix(k,l,L)
     A = zeros(Float64,2^L,2^L)
     for i in 0:2^L-1
@@ -25,30 +29,6 @@ function one_body_to_matrix(k,l,L)
     return A
 end
 
-function undigits(x;base=2)
-    return sum([x[k]*base^(k-1) for k in 1:length(x)])
-end
-
-function sp_one_body_to_matrix(k,l,L,N)
-    Is = spzeros(2^L)
-    Js = spzeros(2^L)
-    Vs = spzeros(2^L)
-    j = 0 #counter
-    for occ_list in combinations(1:L,N)
-        if (l in occ_list) && ((k==l) || !(k in occ_list))
-            j+=1
-            x = [m in occ_list for m in 1:L]
-            Js[j] = undigits(x)+1
-            Vs[j] = (-1)^sum(x[1:l-1])
-            x[l] = 0
-            x[k] = 1
-            Is[j] = undigits(x)+1
-            Vs[j] *= (-1)^sum(x[1:k-1])
-        end
-    end
-    return sparse(Is[1:j],Js[1:j],Vs[1:j],2^L,2^L)
-end
-
 function two_body_to_matrix(k,l,m,n,L)
     A = zeros(Float64,2^L,2^L)
     for i in 0:2^L-1
@@ -61,31 +41,6 @@ function two_body_to_matrix(k,l,m,n,L)
         end
     end
     return A
-end
-
-#assume k<l, m<n
-function sp_two_body_to_matrix(k,l,m,n,L,N)
-    Is = spzeros(2^L)
-    Js = spzeros(2^L)
-    Vs = spzeros(2^L)
-    j = 0 #counter
-    for occ_list in combinations(1:L,N)
-        if (m in occ_list) && (n in occ_list) && ((k==m) || (k==n) || !(k in occ_list)) && ((l==m) || (l==n) || !(l in occ_list))
-            j+=1
-            x = [r in occ_list for r in 1:L]
-            Js[j] = undigits(x)+1
-            Vs[j] = (-1)^sum(x[1:n-1])
-            x[n] = 0
-            Vs[j] *= (-1)^sum(x[1:m-1])
-            x[m] = 0
-            Vs[j] *= (-1)^sum(x[1:l-1])
-            x[l] = 1
-            Vs[j] *= (-1)^sum(x[1:k-1])
-            x[k] = 1
-            Is[j] = undigits(x)+1
-        end
-    end
-    return sparse(Is[1:j],Js[1:j],Vs[1:j],2^L,2^L)
 end
 
 #naive implementation
@@ -114,21 +69,6 @@ function sc_to_mat(h,V)
         end
     end
     return A
-end
-
-#h and V sparse
-function sp_sc_to_mat(h,V,N)
-    L = size(h,1)
-    A = spzeros(2^L,2^L)
-    for i in findall(!iszero,h)
-        H = sp_one_body_to_matrix(i[1],i[2],L,N)
-        A += h[i]*(H+H')
-    end
-    for i in findall(!iszero,V)
-        H = sp_two_body_to_matrix(i[1],i[2],i[3],i[4],L,N)
-        A += V[i]*(H+H')
-    end
-    return dropzeros!(A)
 end
 
 #returns the h,V for the hubbard model
@@ -237,7 +177,7 @@ function test_2body_mpo()
 end
 
 #assuming natural labelling of sites
-function hubbard_1D(L;t=1,U=1)
+function hubbard_1D(L;t=1,U=1, pbc=false)
     h = zeros(2L,2L)
     V = zeros(2L,2L,2L,2L)
     for i in 1:L-1
@@ -245,9 +185,11 @@ function hubbard_1D(L;t=1,U=1)
         h[2i-1,2i+1]=-t
         V[2i-1,2i,2i-1,2i]=-U #because of the anticommutation rules
     end
-    h[2L-1,1]=-t
-    h[2L,2]=-t
     V[2L-1,2L,2L-1,2L]=-U
+    if pbc 
+        h[2L-1,1]=-t
+        h[2L,2]=-t
+    end
     return h,V
 end
 
@@ -314,11 +256,4 @@ function hV_to_mpo(h,V)
         A = tto_add(A,mult_a_tt(V[i],tto_add(H,G)))
     end
     return A
-end
-
-function ham_to_mpo(h,V,N)
-    L = size(h,1)
-    H = sp_sc_to_mat(h,V,N)
-    dim = 2*ones(Int,L)
-    return tto_spdecomp(sparsetensor_mat(dim,dim,H),L)
 end
