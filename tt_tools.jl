d68c1a447713976d200ee721942367c93b07f76c
Mi-Song Dupuy <mi-song.dupuy@tum.de>
2021-03-09 18:22:08 +0100

    QRIteration

--- tt_tools.jl
+++ tt_tools.jl
@@ -275,7 +275,7 @@ function tt_rounding(x_tt::ttvector;tol=1e-14)
 	for j in 1:d-1
 		A = zeros(x_tt.ttv_dims[j],y_rks[j],x_tt.ttv_dims[j+1],y_rks[j+2])
 		@tensor A[a,b,c,d] = y_vec[j][a,b,z]*y_vec[j+1][c,z,d]
-		u,s,v = svd(reshape(A,size(A,1)*size(A,2),:))
+		u,s,v = svd(reshape(A,size(A,1)*size(A,2),:);alg=LinearAlgebra.QRIteration())
 		Σ = s[s.>tol]
 		y_rks[j+1] = length(Σ)
 		y_vec[j] = reshape(u[:,s.>tol],x_tt.ttv_dims[j],y_rks[j],:)
