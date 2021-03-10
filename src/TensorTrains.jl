a1c31b4792db9e9a2f8797ae97074c274e367431
Mi-Song Dupuy <mi-song.dupuy@tum.de>
2021-03-10 10:26:43 +0100

    fixed bug

--- src/TensorTrains.jl
+++ src/TensorTrains.jl
@@ -1,7 +1,7 @@
 module TensorTrains
 
 include("tt_tools.jl")
-export ttvector,ttoperator,ttv_decomp,tto_decomp,tt_up_rks,tto_add,tt_svdvals,tt_dot,tt_add,tt_rounding,mult,mult_a_tt,ttv_to_tensor,tto_to_tensor,tt_compression_par
+export ttvector,ttoperator,ttv_decomp,tto_decomp,tt_up_rks,tto_add,tt_svdvals,tt_dot,tt_add,tt_rounding,mult,mult_a_tt,ttv_to_tensor,tto_to_tensor,tt_compression_par,tt_orthogonalize
 
 include("als.jl")
 export als_linsolv, als_eigsolv, als_gen_eigsolv
@@ -13,10 +13,10 @@ include("tt_solvers.jl")
 export tt_cg, tt_gmres
 
 include("models.jl")
-export hubbard_1D, hubbard_2D, PPP_C_NH_N, hV_to_mpo, site_switch
+export hubbard_1D, hubbard_2D, PPP_C_NH_N, hV_to_mpo, site_switch, half_filling
 
 include("ordering_schemes.jl")
-export fiedler_order, bwpo_entropy, one_prdm
+export fiedler_order, bwpo_order, one_prdm
 
 include("particular_states.jl")
 export random_slater, random_prime_tensor, random_static_correlation_tensor
