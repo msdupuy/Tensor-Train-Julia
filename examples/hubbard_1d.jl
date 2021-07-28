a0f6bcf3a50bab9ed74bc67123a8a45e1b536b72
Mi-Song Dupuy <misong.dupuy@gmail.com>
2021-07-28 09:32:11 +0200

    updated examples

--- examples/hubbard_1d.jl
+++ examples/hubbard_1d.jl
@@ -4,5 +4,4 @@ L=10
 h,V = hubbard_1D(L)
 H_tto = hV_to_mpo(h,V,tol=1e-5)
 ψ_0 = half_filling(L)
-E_als,ψ_tt = als_eigsolv(H_tto,ψ_0,sweep_schedule=[2,5],rmax_schedule=[16,32])
-E_mals,ψ_tt,r = mals_eigsolv(H_tto,ψ_tt,tol=1e-6,sweep_schedule=[2,5],rmax_schedule=[32,48])
+E,ψ_tt,r_dmrg = dmrg_eigsolv(H_tto,ψ_0,sweep_schedule=[2,4,7],rmax_schedule=[16,32,64])
--- examples/hubbard_2d.jl
+++ examples/hubbard_2d.jl
@@ -2,8 +2,7 @@ using TensorTrains
 
 w=2
 L=5
-h,V = hubbard_2D(w,L;U=4)
+h,V = hubbard_2D(w,L;U=1)
 H_tto = hV_to_mpo(h,V,tol=1e-5)
-ψ_0 = half_filling(w*L)
-E_als,ψ_tt = als_eigsolv(H_tto,ψ_0,sweep_schedule=[2,5],rmax_schedule=[16,32])
-E_mals,ψ_tt,r = mals_eigsolv(H_tto,ψ_tt,tol=1e-6,sweep_schedule=[2,5],rmax_schedule=[32,48])
+ψ_0 = tt_up_rks(half_filling(w*L),16;ϵ_wn = 1e-1)
+E,ψ_tt,r_dmrg = dmrg_eigsolv(H_tto,ψ_0,sweep_schedule=[2,4,7],rmax_schedule=[16,32,64])
