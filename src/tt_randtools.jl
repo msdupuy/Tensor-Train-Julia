5799ae7240c3efd95ea3ad4e74f12c17de089e44
Mi-Song <misong.dupuy@gmail.com>
2023-07-26 18:19:15 -0400

    random gaussian TT

---
+++ src/tt_randtools.jl
@@ -0,0 +1,8 @@
+"""
+TT rounding algorithm in https://doi.org/10.1137/21M1451191
+Algorithm 3.2 "Randomize then Orthogonalize"
+"""
+
+function ttrand_rounding(y,r)
+
+end
--- src/tt_tools.jl
+++ src/tt_tools.jl
@@ -101,12 +101,13 @@ end
 """
 Returns a random TTvector with dimensions `dims` and ranks `rks`
 """
-function rand_tt(dims,rks;T=Float64)
+function rand_tt(dims,rks;T=Float64,normalise=false)
 	@assert length(dims)+1==length(rks) "Dimensions and ranks are not compatible"
 	d = length(dims)
 	tt_vec = Array{Array{T,3}}(undef,d)
 	for i in eachindex(tt_vec) 
 		tt_vec[i] = randn(T,dims[i],rks[i],rks[i+1])
+		normalise && tt_vec[i] *=1/(rks[i]*rks[i+1])
 	end
 	return TTvector{T}(d,tt_vec,dims,copy(rks),zeros(Int,d))
 end
