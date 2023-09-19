146536575cbd3a4f6d6806bcb22245641e2d8cbc
Mi-Song <misong.dupuy@gmail.com>
2023-09-19 10:22:56 +0200

    examples in functions

--- examples/hubbard_2d.jl
+++ examples/hubbard_2d.jl
@@ -1,8 +1,9 @@
 using TensorTrains
 
-w=2
-L=5
-h,V = hubbard_2D(w,L;U=1)
-H_tto = hV_to_mpo(h,V,tol=1e-5)
-ψ_0 = tt_up_rks(half_filling(w*L),16;ϵ_wn = 1e-1)
-E,ψ_tt,r_dmrg = dmrg_eigsolv(H_tto,ψ_0,sweep_schedule=[2,4,7],rmax_schedule=[16,32,64])
+function hubbard_2D_dmrg(w,L)
+  h,V = hubbard_2D(w,L;U=1)
+  H_tto = hV_to_mpo(h,V,tol=1e-5)
+  ψ_0 = tt_up_rks(half_filling(w*L),16;ϵ_wn = 1e-1)
+  E,ψ_tt,r_dmrg = dmrg_eigsolv(H_tto,ψ_0,sweep_schedule=[2,4,7],rmax_schedule=[16,32,64])
+  return  E,ψ_tt,r_dmrg
+end
---
+++ examples/hubbard_2d_arnoldi.jl
@@ -0,0 +1,9 @@
+using TensorTrains
+
+function hubbard_2D_arnoldi(w,L;rmax=128,m=3,ε=1e-6,ε_tt=1e-4,Imax=10,r=2,which=:SM,σ=convert(Float64,w*L+2))
+  h,V = hubbard_2D(w,L;U=1)
+  v = rand_tt(2*ones(Int64,2w*L),vcat(1,r*ones(Int64,2w*L-1),1);normalise=true)
+  H_tto = hV_to_mpo(h,V,tol=1e-5)
+  E,ψ_tt =eig_arnoldi(H_tto,m,v;Imax=Imax,ε=ε,ε_tt=ε_tt,rmax=rmax,which=which,σ=σ) 
+  return E,ψ_tt
+end
