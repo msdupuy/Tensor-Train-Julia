46f199b2bbfbc08d038da743cd49eb7a70acfb3f
Mi-Song <misong.dupuy@gmail.com>
2023-10-06 12:16:05 +0200

    new shift

--- examples/Be_arnoldi.jl
+++ examples/Be_arnoldi.jl
@@ -2,13 +2,12 @@
 Transcorrelated Beryllium using Arnoldi
 """
 
-function Be_arnoldi(Imax;m=3,rmax=64,ε=1e-5,ε_tt=1e-5,σ=15.0,r=2)
+function Be_arnoldi(Imax;m=3,rmax=64,ε=1e-5,ε_tt=1e-5,σ=20.0,r=2)
   F = read_electron_integral_tensors("./examples/Be.ezfio.FCIDUMP")
   int_1e,int_2e = F[4],F[5]
   v = slater(F[3],2F[2])
   h,V = one_e_two_e_integrals_to_hV(int_1e,int_2e)
-  #V = permutedims(V,(1,3,4,2))
-  H_tto = TensorTrains.hV_no_to_mpo(h,V,ntuple(x->2,2F[2]),tol=1e-5,n=F[3])
+  H_tto = TensorTrains.hV_to_mpo(h,V,ntuple(x->2,2F[2]),tol=1e-5) #,n=F[3]
   E,ψ_tt =eig_arnoldi(H_tto,m,v;Imax=Imax,ε=ε,ε_tt=ε_tt,rmax=rmax,which=:SR,σ=σ) 
   return E,ψ_tt
 end
