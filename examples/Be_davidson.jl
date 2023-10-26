2d83edd82f2ddf71e7e54b2fba3e3f5bdc09fa40
Mi-Song <misong.dupuy@gmail.com>
2023-10-26 16:08:26 +0200

    davidson for Be

--- examples/Be_davidson.jl
+++ examples/Be_davidson.jl
@@ -15,6 +15,6 @@ function Be_davidson(Imax;m=3,rmax=64,ε=1e-5,ε_tt=1e-5,σ=16.0)
   h,V = one_e_two_e_integrals_to_hV(int_1e,int_2e)
   H_tto = TensorTrains.hV_no_to_mpo(h,V,ntuple(x->2,2F[2]),tol=1e-10) #,n=F[3]
   prec = one_body_diagonal(diag(h),σ=σ)
-  λ,ψ,res = davidson(H_tto,m,v;Imax=Imax,ε=ε,ε_tt=ε_tt,rmax=min(24,rmax),which=:SR,σ=σ,prec=prec)
+  λ,ψ,res = davidson(H_tto,m,v;Imax=Imax,ε=ε,ε_tt=ε_tt,rmax=rmax,which=:SR,σ=σ,prec=prec)
   return λ,ψ,res
 end
