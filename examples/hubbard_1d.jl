e287d05acee10ac223463bfb21cc22a2dcd3301a
Mi-Song <misong.dupuy@gmail.com>
2024-02-01 14:26:36 +0100

    bug in calling hV_to_mpo

--- examples/hubbard_1d.jl
+++ examples/hubbard_1d.jl
@@ -1,8 +1,8 @@
 using TensorTrains
 
 L=10
-dims = ntuple(x->2,L)
+dims = ntuple(x->2,2L)
 h,V = hubbard_1D(L)
-H_tto = hV_to_mpo(h,V,dims,tol=1e-5)
+H_tto = hV_to_mpo(Matrix(h),V,dims,tol=1e-5)
 ψ_0 = half_filling(L)
 E,ψ_tt,r_dmrg = dmrg_eigsolv(H_tto,ψ_0,sweep_schedule=[2,4,7],rmax_schedule=[16,32,64])
