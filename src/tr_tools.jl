16b4a1ceb7defdd185080ea5923b4a6210996e72
Mi-Song <misong.dupuy@gmail.com>
2024-04-18 16:37:53 +0200

    update tr_tools

--- src/tr_tools.jl
+++ src/tr_tools.jl
@@ -8,18 +8,24 @@ import Base.copy
 """
 Tensor rings (TR), aka periodic MPS. TR are stored in 'vec' as order three tensors of size A_k ∈ R^{n_k × r_{k-1} × r_k} with r_0 = r_d.
 """
-struct TRvector{T<:Number}
+abstract type AbstractTRvector end
+
+struct TRvector{T<:Number,M} <:AbstractTRvector
 	N :: Int
 	ttv_vec :: Array{Array{T,3},1}
-	ttv_dims :: Vector{Int64}
+	ttv_dims :: NTuple{M,Int64}
 	ttv_rks :: Array{Int64,1}
 	ttv_ot :: Array{Int64,1}
+	function TRvector{T,M}(N,vec,dims,rks,ot) where {T,M}
+		@assert M isa Int64
+		new{T,M}(N,vec,dims,rks,ot)
+	end
 end
 
-Base.eltype(::TRvector{T}) where {T<:Number} = T 
+Base.eltype(::TRvector{T,N}) where {T<:Number,N} = T 
 
 #returns the tensor represented by 'x_tr'
-function tr_to_tensor(x_tr::TRvector{T}) where T<:Number
+function tr_to_tensor(x_tr::TRvector{T,N}) where {T<:Number,N}
 	d = x_tr.N
 	# Initialize the to be returned tensor
 	tensor = zeros(T, x_tr.ttv_dims...)
@@ -38,43 +44,34 @@ function tr_to_tensor(x_tr::TRvector{T}) where T<:Number
 end
 
 #returns a zero TRvector
-function zeros_tr(dims,rks;T=Float64)
+function zeros_tr(::Type{T},dims::NTuple{N,Int64},rks) where {T,N}
 	@assert length(rks)==length(dims)+1
-	d = length(dims)
-	vec = Array{Array{T,3},1}(undef,d)
-	for i in eachindex(vec)
-		vec[i] = zeros(dims[i],rks[i],rks[i+1])
-	end
-	return TRvector{T}(d,vec,dims,rks,zeros(Int,d))
+	vec = [zeros(T,dims[i],rks[i],rks[i+1]) for i in eachindex(dims)]	
+	return TRvector{T,N}(N,vec,dims,deepcopy(rks),zeros(Int,N))
 end
 
-function Base.copy(x_tr::TRvector{T}) where T<:Number
-	return TRvector{T}(x_tr.N,copy(x_tr.ttv_vec),copy(x_tr.ttv_dims),copy(x_tr.ttv_rks),copy(x_tr.ttv_ot))
+function Base.copy(x_tr::TRvector{T,N}) where {T<:Number,N}
+	return TRvector{T,N}(x_tr.N,copy(x_tr.ttv_vec),x_tr.ttv_dims,copy(x_tr.ttv_rks),copy(x_tr.ttv_ot))
 end
 
 """
 Returns a random 'TRvector' of dimensions 'dims' and ranks 'rks'
 """
-function rand_tr(dims,rks;T=Float64)
+function rand_tr(dims,rks)
+	return rand_tr(Float64,dims,rks)
+end
+
+function rand_tr(::Type{T},dims,rks) where T
 	@assert length(dims)+1==length(rks) "Incompatible dimensions and ranks"
 	@assert rks[1] == rks[end] "Incompatible first and last ranks"
 	d = length(dims)
-	tt_vec = Array{Array{T,3}}(undef,d)
-	for i in eachindex(tt_vec) 
-		tt_vec[i] = randn(T,dims[i],rks[i],rks[i+1])
-	end
-	return TRvector{T}(d,tt_vec,dims,copy(rks),zeros(Int,d))
+	tt_vec = [randn(T,dims[i],rks[i],rks[i+1]) for i in eachindex(dims)]
+	return TRvector{T,d}(d,tt_vec,dims,copy(rks),zeros(Int64,d))
 end
 
 """
 Returns a random 'TRvector' of dimensions 'dims' and maximal rank 'rmax'
 """
-function rand_tr(dims,rmax::Int;T=Float64)
-	d = length(dims)
-	rks = rmax*ones(Int,d+1)
-	tt_vec = Array{Array{T,3}}(undef,d)
-	for i in eachindex(tt_vec) 
-		tt_vec[i] = randn(T,dims[i],rmax,rmax)
-	end
-	return TRvector{T}(d,tt_vec,dims,rks,zeros(Int,d))
+function rand_tr(dims,rmax::Int)
+	return rand_tr(dims,rmax*ones(Int,length(dims)+1))
 end
