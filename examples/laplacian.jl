a05042518fee5cfbb23a011490b601aab0a899f4
Mi-Song <misong.dupuy@gmail.com>
2024-07-12 11:38:23 +0200

    fix bug

--- examples/laplacian.jl
+++ examples/laplacian.jl
@@ -9,7 +9,7 @@ const d = 4
 
 #b_tt = b_ttv(n,d)
 const r = 2
-const b_tt = rand_tt(n*ones(Int64,d),vcat(1,r*ones(Int64,d-1),1))
+const b_tt = rand_tt(ntuple(x->n,d),vcat(1,r*ones(Int64,d-1),1))
 const H_mpo = Δ_tto(n,d)
 
 const α_opt = 2/(d*eigmax(Δ(n))+d*eigmin(Δ(n)))
@@ -22,19 +22,19 @@ const n = 10
 const d = 10
 #b_tt = b_ttv(n,d)
 const r = 2
-const b_tt = rand_tt(n*ones(Int64,d),vcat(1,r*ones(Int64,d-1),1))
-const H_mpo = Δ_tto(n,d)
+const b2_tt = rand_tt(ntuple(x->n,d),vcat(1,r*ones(Int64,d-1),1),normalise=true)
+const H2_mpo = Δ_tto(n,d)
 
 const Imax = 100
 const α_opt = 2/(d*eigmax(Δ(n))+d*eigmin(Δ(n)))
 const i_trunc = 1
-#eps_list = [1e-2]
-const eps_list = [1e-2,1e-3,1e-4,1e-5,1e-6]
+eps_list = [1e-2]
+#const eps_list = [1e-2,1e-3,1e-4,1e-5,1e-6]
 const res_list = zeros(length(eps_list),Imax+1)
 const sol_hsvd = Array{Array{Array{Float64,1}}}(undef,length(eps_list))
 
 for i_ε in eachindex(eps_list)
-  x_ϵ_tt, res_ϵ = gradient_fixed_step(H_mpo,b_tt,α_opt,Imax=Imax,i_trunc=1,eps_tt=eps_list[i_ε])
+  x_ϵ_tt, res_ϵ = gradient_fixed_step(H2_mpo,b2_tt,α_opt,Imax=Imax,i_trunc=1,eps_tt=eps_list[i_ε])
   println(x_ϵ_tt.ttv_rks)
   Σ = tt_svdvals(x_ϵ_tt)
   sol_hsvd[i_ε] = Σ
