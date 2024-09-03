0f666b32e8c5a77f2cd7f464b48e2fd709715158
Mi-Song <misong.dupuy@gmail.com>
2024-09-03 15:05:39 +0200

    test LiH

--- examples/LiH.jl
+++ examples/LiH.jl
@@ -9,7 +9,7 @@ function lih(;n_cas = 10, rmax=64,ε=1e-5,ε_tt=1e-5,fiedler=false,history=true,
   v = tt_up_rks(v,64;ϵ_wn=1e-2)
   h,V = one_e_two_e_integrals_to_hV(int_1e[1:n_cas,1:n_cas],int_2e[1:n_cas,1:n_cas,1:n_cas,1:n_cas])
   H_tto = TensorTrains.hV_no_to_mpo(h,V,ntuple(x->2,2n_cas),tol=1e-10) #,n=F[3]
-  E,ψ_tt,r_hist =dmrg_eigsolv(H_tto,v;sweep_schedule=[4],rmax_schedule=[64],it_solver=true) 
+  E,ψ_tt,r_hist =dmrg_eigsolv(H_tto,v;sweep_schedule=[4,8,12,16],rmax_schedule=[32,64,96,128],it_solver=true) 
   if print
     date = now()
     open("psi_LiH_canonical_cas=$(n_cas)_$(date).json","w") do io 
@@ -23,7 +23,9 @@ function lih(;n_cas = 10, rmax=64,ε=1e-5,ε_tt=1e-5,fiedler=false,history=true,
     end
   end
   if fiedler
-    imaxmax += 1
+    v = slater(F[3],2n_cas)
+    v = tt_up_rks(v,64;ϵ_wn=1e-2)
+    E,ψ_tt,r_hist =dmrg_eigsolv(H_tto,v;sweep_schedule=[4],rmax_schedule=[48]) 
     perm = fiedler_order(ψ_tt)
     date = now()
     if print
@@ -35,18 +37,17 @@ function lih(;n_cas = 10, rmax=64,ε=1e-5,ε_tt=1e-5,fiedler=false,history=true,
     v = tt_up_rks(v,64;ϵ_wn=1e-2)
     h,V = h[perm,perm], V[perm,perm,perm,perm]
     H_tto = TensorTrains.hV_no_to_mpo(h,V,ntuple(x->2,2n_cas),tol=1e-10)
-    E,ψ_tt,r_hist =dmrg_eigsolv(H_tto,v;sweep_schedule=[4],rmax_schedule=[128],it_solver=true) 
+    E,ψ_tt,r_hist =dmrg_eigsolv(H_tto,v;sweep_schedule=[4,8,12,16],rmax_schedule=[32,64,96,128],it_solver=true) 
     if print
-      open("psi_cas=$(n_cas)_$(date).json","w") do io 
+      open("psi_LiH_fiedler_cas=$(n_cas)_$(date).json","w") do io 
         JSON3.pretty(io,ψ_tt)
       end
-      open("E_cas=$(n_cas)_$(date).json","w") do io 
+      open("E_LiH_fiedler_cas=$(n_cas)_$(date).json","w") do io 
         JSON3.pretty(io,E)
       end
-      open("hist_cas=$(n_cas)_$(date).json","w") do io
+      open("hist_LiH_fiedler_cas=$(n_cas)_$(date).json","w") do io
         JSON3.pretty(io,r_hist)
       end
     end
   end
-  return E,ψ_tt
 end
