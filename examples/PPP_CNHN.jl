046ac61d4ab2e11a2ed6e102ea387d5ccec1088b
Mi-Song <misong.dupuy@gmail.com>
2025-07-31 18:21:38 +0200

    updated

--- examples/PPP_CNHN.jl
+++ examples/PPP_CNHN.jl
@@ -7,6 +7,7 @@ E_dmrg ≃ -0.46752535023652453
 
 N = 6
 H_tto = PPP_C_NH_N(N)
-ψ_0 = tt_up_rks(half_filling(N),32;ϵ_wn=1e-2)
+ψ_0 = tt_up_rks(half_filling(N),32;ϵ_wn=1e-1)
 ψ_0 = orthogonalize(ψ_0)
-E_dmrg, ψ_tt, r_dmrg = dmrg_eigsolv(H_tto,ψ_0)
+schedule = dmrg_schedule_default(;N=2,rmax=64,nsweeps=2,it_solver=true)
+E_dmrg, ψ_tt, dmrg_info = dmrg_eigsolv(H_tto,ψ_0;schedule)
